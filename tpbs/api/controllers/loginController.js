9fef9e6909e36d2a2f4c4e60937ce47d85f2abbe
LuatPT <luatphamdhv@gmail.com>
2020-06-08 22:58:30 +0700

    20200608
    Souce code 20200608

--- tpbs/api/controllers/loginController.js
+++ tpbs/api/controllers/loginController.js
@@ -1,13 +1,13 @@
-"use strict";
-const LoginModel = require("../models/loginModel")
+'use strict';
+const LoginModel = require('../models/loginModel');
 exports.login_account = (req, res) => {
   var username = req.body.uid;
   var password = req.body.pwd;
-  console.log(req.body)
+  console.log(req.body);
   LoginModel.authenciate({ username, password }, (err, exist) => {
     if (err) res.send(err);
-    console.log(exist)
+    console.log(exist);
     // if (exist) res.json({ message: exist })
     res.json(exist);
-  })
-}
+  });
+};
